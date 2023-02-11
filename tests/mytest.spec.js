e94dbacfb848131c732eec39d7b75925647b080c
Yuri Andorschuk <yuan@opinov8.com>
2023-02-11 17:49:20 +0200

    fixing stuff

--- tests/mytest.spec.js
+++ tests/mytest.spec.js
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('Testing google', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+//   await page.pause()
   await page.locator('[name=q]').click()
   await page.locator('[name=q]').fill('this is a test')
   await page.locator('[name=q]').press('Enter')
   
   //Here we're typing instead of filling (fun!)
   await page.locator('[name=q]').type('NOOOOOOOOO THIS IS THE TEST')
+
 if (await page.getByText('Увійти').isDisabled()) {
     await page.getByRole('link', { name: 'Google' }).click()
 }
