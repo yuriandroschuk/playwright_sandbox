f8fd0db59db1d13e1827493f26f38a1aa8f272b2
Yuri Andorschuk <yuan@opinov8.com>
2023-02-11 17:29:09 +0200

    first commit

---
+++ tests/mytest.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('Testing google', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  if(page.getByRole('button', { name: 'Ні, дякую' }).isVisible) {
+     page.getByRole('button', { name: 'Ні, дякую' }).click()
+   }
+//   await page.pause()
+  await page.locator('[name=q]').click()
+  await page.locator('[name=q]').fill('this is a test')
+  await page.locator('[name=q]').press('Enter')
+  
+  //Here we're typing instead of filling (fun!)
+  await page.locator('[name=q]').type('NOOOOOOOOO THIS IS THE TEST')
+if (await page.getByText('Увійти').isDisabled()) {
+    await page.getByRole('link', { name: 'Google' }).click()
+}
+else {
+    await page.getByText('Увійти').click()
+  }
+  await expect(page).toHaveURL(/accounts.google.com/)
+});
